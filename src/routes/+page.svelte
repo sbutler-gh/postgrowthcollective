7d2a374cc274ba4af36b50e7c751a3b10b58a6ff
sbutler-gh <sam@ysli.be>
2022-10-28 14:01:11 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,166 @@
+<h1>Post-Growth Collective</h1>
+<p>A collective/cooperative of people, groups, and communities, interested in making new worlds real.</p>
+<p>Want to join or stay updated?</p>
+
+<form>
+    <label>Name, or what you'd like to be called</label>
+    <input placeholder="Ursula Le Guin, Shevek, X Æ A-12 Musk">
+
+    <label>Email* (the only required item)</label>
+    <input>
+
+    <label>Phone number (this helps for connecting!)</label>
+    <input>
+
+    <label>What activities would you like to see a post-growth collective focus on?</label>
+<textarea placeholder="E.g. Working on transitions in housing, transportation, food, energy, public services, in real-world communities
+
+Investing in the building blocks of post-growth transitions
+
+Creating services and convivial tools
+
+Disseminating content and raising awareness of postgrowth
+
+Bringing to life stories, music, and cultural initiatives
+
+"></textarea>
+
+<!-- <label>What groups, Substacks, clubs, communities are you part of, that could be interested in being part of a post-growth collective? (Seperate by commas)</label>
+<textarea placeholder="E.g. Soapbox project, Heated, 
+
+"></textarea> -->
+
+    <label>There is a beautiful plurality across post-growth aligned movements.</label>
+    <label>Are there any groups, initiatives, or communities you're part of, that could be interested in joining a post-growth collective?</label>
+<textarea placeholder="e.g. I have a newsletter subscriber community
+
+I lead a bike advocacy group in my city
+
+My peers and I at university have an academic club that would like to join.
+
+We, the 800-person citizenry of Vatican City, would like to join the post-growth collective as a sovereign nation.
+
+"></textarea>     
+        <!-- re there any groups or communities that you lead or are part of, that you would like to join the collective?  (e.g. IRL group, Research network, Newsletter community?)</label> -->
+<!-- <textarea placeholder="e.g. I have a newsletter subscriber community, and they would probably like to be involved in this.
+
+I lead a bike advocacy group in my city, and people would be onboard.
+
+My peers and I at university have an academic club that would like to join.
+
+We, the 800-person citizenry of Vatican City, would like to join the post-growth collective as a sovereign nation.
+
+"></textarea> -->
+
+    <label>What are your favorite post-growth inspirations?  (Ideas, research papers, sci-fi books, quotes, real-world movements, postgrowth scenarios)</label>
+<textarea placeholder="e.g. 'Decent living with minimum energy: a global scenario' research paper
+
+'A world where many worlds fit'
+
+The writing and philosophies of Ursula Le Guin
+
+Indigenous philosophies on relationality from Turtle Island
+
+Avalon film by Barry Levinson (watched in reverse is a post-growth transition!)
+
+Zapatistas in Mexico
+
+Research and Degrowth community
+
+Twitter postgrowth / degrowth community
+
+'Tools for Conviviality' by Ivan Illich
+
+
+
+"></textarea>
+
+    <label>City</label>
+    <input>
+
+    <label>Country</label>
+    <input>
+
+    <label>Twitter handle (while it lasts ...)</label>
+    <input placeholder="">
+
+    <label>Are you okay with any of these responses being public? (So people can read them, and we can put ourselves on a map or something!)</label>
+    <div style="padding: 1em;">
+    <div class="checkbox">
+    <span>My name can be public</span>
+    <input type="checkbox">
+    </div>
+    <div class="checkbox">
+    <label>My email can be public</label>
+    <input type="checkbox">
+    </div>
+    <div class="checkbox">
+    <label>My activities response can be public</label>
+    <input type="checkbox">
+    </div>
+    <div class="checkbox">
+    <label>My groups/communities response can be public</label>
+    <input type="checkbox">
+    </div>
+    <div class="checkbox">
+    <label>My inspirations response can be public</label>
+    <input type="checkbox">
+    </div>
+    <div class="checkbox">
+    <label>My city can be public</label>
+    <input type="checkbox">
+    </div>
+    <div class="checkbox">
+    <label>My country can be public</label>
+    <input type="checkbox">
+    </div>
+    <div class="checkbox">
+    <label>My Twitter handle can be public</label>
+    <input type="checkbox">
+    </div>
+
+        </div>
+
+    <em style="text-align: center; display: block;">"The means is the end.  But what if there is no end?  Then there's only the means."</em>
+    <br>
+    <button type="submit">Submit</button>
+</form>
+
+<style>
+    form > label, input {
+        display: block;
+    }
+
+    form {
+            /* margin: auto; */
+            width: 500px;
+            max-width: 100%;
+            margin-top: 1em;
+            padding: 1em;
+        }
+
+    textarea {
+        max-width: 100%;
+        width: 460px;
+        height: 100px;
+    }
+
+    input {
+        width: 300px;
+    }
+    
+    input, textarea {
+        margin-bottom: 1em;
+    }
+
+    div.checkbox > * {
+        display: inline-block;
+        width: min-content;
+        white-space: nowrap;
+    }
+
+    button[type="submit"] {
+        margin: auto;
+        display: block;
+    }
+</style>
